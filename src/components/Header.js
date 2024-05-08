48122d5510300b8775699b7ab855b3c3b5bec8fe
bhagyaa2002 <bhagyaammenadka@gmail.com>
2024-05-08 17:22:50 +0530

    sign in/sign up form && Login/header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img className="w-34" 
+      src="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png" 
+      alt="logo" 
+      />
+    </div>
+  )
+}
+
+export default Header
